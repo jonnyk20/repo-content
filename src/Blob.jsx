0fa2070acd828c0d18b2b7404b9f6830183bb9b0
Jonathan Kalambay <jonnyk20@gmail.com>
2017-11-15 19:34:38 -0800

    rendering nested blobs

---
+++ src/Blob.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Blob = (props) => {
+ return <li> 
+   Blob -- Path: {props.item.path}, Type: {props.item.type}, parentBlob: {props.item.parentTree} 
+   
+   </li>
+}
+
+export default Blob;
---
+++ src/Tree.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tree = (props) => {
+ return <li> 
+    Tree -- Path: {props.item.path}, Type: {props.item.type}, parentTree: {props.item.parentTree}
+    <ul>
+      {props.renderTree(props.item.items)}
+    </ul>
+   </li>
+}
+
+export default Tree;
