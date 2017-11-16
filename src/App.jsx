d1793cd8874bca2760d6256a234838ec769410ff
Jonathan Kalambay <jonnyk20@gmail.com>
2017-11-15 18:09:06 -0800

    repo items saved to file and listed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  state = {
+    repo: []
+  }
+  renderRepo = () => {
+    return this.state.repo.map( item => <li key={item.sha}> {item.path} </li>)
+  }
+  handleClick = () => {
+    const repo = [];
+    fetch('https://api.github.com/repos/jonnyk20/repo-content/git/trees/master?recursive=1')
+      .then(res => res.json())
+        .then( data => {
+          data.tree.forEach((item) => repo.push(item));
+          this.setState({
+            repo
+          })
+        })
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>Repository Contents</h1>
+        <button onClick={this.handleClick}>Get Repo Tree</button>
+        <div> Repository Tree </div>
+        <ul>
+          { this.renderRepo() }
+        </ul>
+      </div>
     );
   }
 }
